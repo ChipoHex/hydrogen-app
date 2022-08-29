490b7c903cf473ffd79e3bd93adf0a175fb24b8c
Hex <tec@Hex6666.local>
2022-08-29 01:56:25 +0100

    first commit

---
+++ hydrogen.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'kicksnake-nextjs.myshopify.com',
+    storefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      'd8502c31946bb33ca249439b394dbb9d',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
